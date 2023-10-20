4c3b41f10c19071284544e29b6378ab48002d237
Ava <avamir80@gmail.com>
2023-10-20 15:53:26 +0330

    add test folder

---
+++ CA1/src/test/java/model/CommentTest.java
@@ -0,0 +1,18 @@
+package model;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class CommentTest {
+
+
+//    @Test
+//    void testCurrentDate () {
+//        Date currentDate = new Date();
+//        System.out.println(currentDate);
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        System.out.println(dateFormat.format(currentDate));
+//    }
+}
